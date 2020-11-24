5cfc6c44bdba880892723d2e380434fd5b434a8e
Brett Chapin <brett.chapin89@gmail.com>
2020-11-23 21:25:35 -0500

    Update Config
    Now using the Heroku PostgreSQL database environment

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,7 +7,15 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-    app.databases.use(.postgres(hostname: "localhost", username: "postgres", password: "", database: "moviesdb"), as: .psql)
+//    app.databases.use(.postgres(hostname: "localhost", username: "postgres", password: "", database: "moviesdb"), as: .psql)
+    
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        app.databases.use(try .postgres(
+            url: databaseURL
+        ), as: .psql)
+    } else {
+        // ...
+    }
     
     app.migrations.add(CreateMovie())
     app.migrations.add(CreateReview())
